bc89932126a079f929e237b6aef4ca88192ccad5
j-moreno-c-r <joaomcr@proton.me>
2025-06-15 12:50:53 -0300

    Only for studies

---
+++ README.md
@@ -0,0 +1,13 @@
+## Here some useful tools to learn how the things works....Be careful and respectful
+
+
+start with 
+```bash  
+nix develop --impure
+```
+
+To easy acess to rockyou list:
+```bash  
+cp  $(wordlists_path)/rockyou.txt ./
+```
+ 
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ...} @ inputs: {
+    nixosConfigurations.joaozinho = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      system = "x86_64-linux";
+    };
+
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      packages = with nixpkgs.legacyPackages.x86_64-linux; [
+        sherlock
+        john
+        rockyou
+        sqlmap
+        wordlists
+        hashcat
+        thc-hydra
+        nmap
+        wpscan
+        aircrack-ng
+        metasploit
+        maltego
+        audiness
+        apk-tools
+        snort
+      ];
+    };
+  };
+}
